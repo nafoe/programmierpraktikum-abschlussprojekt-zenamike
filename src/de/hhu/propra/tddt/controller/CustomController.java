0e6eae1d80c8e62965e3e384c6cea89ff3c6df17
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-06-23 20:50:06 +0200

    Created the directory structure of the project on the basis of the division of the CRC cards into packages. To make git upload the directories I hat to create atleast one file in each directory.

---
+++ src/de/hhu/propra/tddt/controller/CustomController.java
@@ -0,0 +1,7 @@
+package de.hhu.propra.tddt.controller;
+
+/**
+ * Created by zeljko On 23.06.2016
+ */
+public class CustomController {
+}
---
+++ src/de/hhu/propra/tddt/cycle/Cycle.java
@@ -0,0 +1,7 @@
+package de.hhu.propra.tddt.cycle;
+
+/**
+ * Created by zeljko On 23.06.2016
+ */
+public class Cycle {
+}
