dd0373a624a820f35c27b4006be292a9ae7c058e
Zeljko Bekcic <zebek101@uni-duesseldorf.de>
2016-06-24 22:58:57 +0200

    Forgot to add what influenced my idea of the Plugin interface. Added Link to a wiki which I read.

--- src/de/hhu/propra/tddt/plugin/Plugin.java
+++ src/de/hhu/propra/tddt/plugin/Plugin.java
@@ -4,6 +4,12 @@ package de.hhu.propra.tddt.plugin;
  * Created by zeljko On 23.06.2016
  */
 
+/*
+ * The link which influenced this and my idea of a Plugin (and probably the
+ * PluginManager) is this one:   
+ * https://wiki.byte-welt.net/wiki/Java-Programme_durch_PlugIns_erweitern
+ */
+
 /******************************************************************************
  * Interface: Plugin
  * <p>
@@ -34,12 +40,12 @@ public interface Plugin {
 
     /**************************************************************************
      * Method: setPluginManager
-     *
+     * <p>
      * To let the Plugin interact with the program we need to give something
      * with what it can work. The PluginManager is the best 'something' because
-     * it knows what kind of information the Plugins are allowed to get. And with
-     * the plugin manager We have specified what information the plugins are
-     * allowed to use.
+     * it knows what kind of information the Plugins are allowed to get. And
+     * with the plugin manager We have specified what information the plugins
+     * are allowed to use.
      **************************************************************************/
     public void setPluginManager(PluginManager pluginManager);
 }
